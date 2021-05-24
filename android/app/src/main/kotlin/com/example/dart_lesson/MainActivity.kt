cbfba029fea92064c4f6045bc939289ef3c43804
jonfir <yolchev@gmsil.com>
2021-05-24 13:57:04 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/dart_lesson/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dart_lesson
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/resources/app_images.dart
@@ -0,0 +1,5 @@
+part of 'resources.dart';
+
+class AppImages {
+  AppImages._();
+}
