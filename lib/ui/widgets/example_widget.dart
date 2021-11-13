0304bcfd983bc3e1d60e9afaae1e8c8187b367cd
jonfir <yolchev@gmsil.com>
2021-11-13 16:10:09 +0700

    add flutter bloc

--- lib/ui/widgets/example_widget.dart
+++ lib/ui/widgets/example_widget.dart
@@ -1,21 +1,27 @@
 import 'package:dart_lesson/domain/blocs/users_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 class ExampleWidget extends StatelessWidget {
   const ExampleWidget({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: const [
-              _AgeTitle(),
-              _AgeIncrementWidget(),
-              _AgeDecrementWidget(),
-            ],
+    return BlocListener<UsersBloc, UsersState>(
+      listener: (context, state) {
+        print(state.currentUser.age);
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                _AgeTitle(),
+                _AgeIncrementWidget(),
+                _AgeDecrementWidget(),
+              ],
+            ),
           ),
         ),
       ),
@@ -28,15 +34,8 @@ class _AgeTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final bloc = context.read<UsersBloc>();
-    return StreamBuilder<UsersState>(
-      initialData: bloc.state,
-      stream: bloc.stream,
-      builder: (context, snapshot) {
-        final age = snapshot.requireData.currentUser.age;
-        return Text("$age");
-      },
-    );
+    final age = context.select((UsersBloc b) => b.state.currentUser.age);
+    return Text("$age");
   }
 }
 
--- lib/ui/widgets/my_app.dart
+++ lib/ui/widgets/my_app.dart
@@ -1,7 +1,7 @@
 import 'package:dart_lesson/domain/blocs/users_bloc.dart';
 import 'package:dart_lesson/ui/widgets/example_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -13,10 +13,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Provider<UsersBloc>(
+      home: BlocProvider<UsersBloc>(
         create: (_) => UsersBloc(),
         child: const ExampleWidget(),
-        dispose: (_, bloc) => bloc.close(),
       ),
     );
   }
