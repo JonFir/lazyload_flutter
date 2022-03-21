8e71f2cfe0648dfabfe3db2240e0d80d10fce1b3
jonfir <yolchev@gmail.com>
2022-03-21 16:14:33 +0700

    TweenAnimationBuilder

--- lib/ui/widgets/example_widget.dart
+++ lib/ui/widgets/example_widget.dart
@@ -1,7 +1,4 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 const duration = Duration(milliseconds: 250);
 
@@ -30,21 +27,7 @@ class _ExampleWidgetState extends State<ExampleWidget> {
           child: Container(
             padding: const EdgeInsets.all(40),
             color: Colors.black,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                // AnimatedDefaultTextStyleExample(toggle: toggle),
-                // AnimatedAlignExample(toggle: toggle),
-                // AnimatedCrossFadeExample(toggle: toggle),
-                // AnimatedOpacityExample(toggle: toggle),
-                // AnimatedPaddingExample(toggle: toggle),
-                // AnimatedRotationExample(toggle: toggle),
-                // AnimatedSwitcherExample(toggle: toggle),
-                AnimatedSizeExample(toggle: toggle),
-                // AnimatedContainerExample(toggle: toggle),
-                // ContainerExample(toggle: toggle),
-              ],
-            ),
+            child: TweenAnimationBuilderExample(toggle: toggle),
           ),
         ),
       ),
@@ -52,255 +35,65 @@ class _ExampleWidgetState extends State<ExampleWidget> {
   }
 }
 
-// AnimatedDefaultTextStyle
-
-class AnimatedDefaultTextStyleExample extends StatelessWidget {
+class TweenAnimationBuilderExample extends StatelessWidget {
   final bool toggle;
-  const AnimatedDefaultTextStyleExample({
+  static final _forwardTween = ColorTween(
+    begin: Colors.white,
+    end: Colors.red,
+  );
+  static final _reverseTween = ReverseTween(_forwardTween);
+
+  const TweenAnimationBuilderExample({
     Key? key,
     required this.toggle,
   }) : super(key: key);
 
-  static const styleOne = TextStyle(fontSize: 30, color: Colors.red);
-  static const styleTwo = TextStyle(fontSize: 13, color: Colors.green);
-
   @override
   Widget build(BuildContext context) {
-    return AnimatedDefaultTextStyle(
+    return TweenAnimationBuilder(
+      tween: toggle ? _forwardTween : _reverseTween,
       duration: duration,
-      style: toggle ? styleOne : styleTwo,
-      child: const Text("Hello world"),
+      builder: (BuildContext context, Color? color, Widget? child) {
+        return ColorFiltered(
+          child: child,
+          colorFilter: ColorFilter.mode(color!, BlendMode.modulate),
+        );
+      },
+      child: Image.asset('images/sun.jpg'),
     );
   }
 }
 
-//AnimatedAlign
-
-class AnimatedAlignExample extends StatelessWidget {
+class TweenAnimationBuilderSecondExample extends StatelessWidget {
   final bool toggle;
-  const AnimatedAlignExample({
+  static final _forwardTween = Tween(
+    begin: 0.0,
+    end: 1.0,
+  );
+  static final _reverseTween = ReverseTween(_forwardTween);
+
+  const TweenAnimationBuilderSecondExample({
     Key? key,
     required this.toggle,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 100,
-      child: AnimatedAlign(
-        duration: duration,
-        alignment: toggle ? Alignment.topLeft : Alignment.bottomRight,
-        child: Container(
-          width: 50,
-          height: 50,
-          color: Colors.red,
-        ),
-      ),
-    );
-  }
-}
-
-//AnimatedCrossFade
-
-class AnimatedCrossFadeExample extends StatelessWidget {
-  final bool toggle;
-  const AnimatedCrossFadeExample({
-    Key? key,
-    required this.toggle,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedCrossFade(
+    return TweenAnimationBuilder(
+      tween: toggle ? _forwardTween : _reverseTween,
       duration: duration,
-      crossFadeState:
-          toggle ? CrossFadeState.showFirst : CrossFadeState.showSecond,
-      firstChild: Container(
-        width: 50,
-        height: 50,
+      builder: (BuildContext context, double value, Widget? child) {
+        return Opacity(
+          opacity: value,
+          child: SizedBox(
+            width: 200 * value,
+            height: 200 * value,
+            child: child,
+          ),
+        );
+      },
+      child: Container(
         color: Colors.red,
-      ),
-      secondChild: Container(
-        width: 150,
-        height: 150,
-        color: Colors.green,
-      ),
-    );
-  }
-}
-
-//AnimatedOpacity
-
-class AnimatedOpacityExample extends StatelessWidget {
-  final bool toggle;
-  const AnimatedOpacityExample({Key? key, required this.toggle})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedOpacity(
-      duration: duration,
-      opacity: toggle ? 1 : 0.5,
-      child: Container(
-        width: 50,
-        height: 50,
-        color: Colors.red,
-      ),
-    );
-  }
-}
-
-// AnimatedPadding
-
-class AnimatedPaddingExample extends StatelessWidget {
-  final bool toggle;
-  const AnimatedPaddingExample({
-    Key? key,
-    required this.toggle,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedPadding(
-      duration: duration,
-      padding: toggle ? EdgeInsets.zero : const EdgeInsets.all(50),
-      child: Container(
-        width: 50,
-        height: 50,
-        color: Colors.red,
-      ),
-    );
-  }
-}
-
-// AnimatedRotation
-
-class AnimatedRotationExample extends StatelessWidget {
-  final bool toggle;
-  const AnimatedRotationExample({Key? key, required this.toggle})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedRotation(
-      duration: duration,
-      turns: toggle ? pi : pi / 2,
-      child: Container(
-        width: 50,
-        height: 50,
-        color: Colors.red,
-      ),
-    );
-  }
-}
-
-//AnimatedSwitcher
-
-class AnimatedSwitcherExample extends StatelessWidget {
-  final bool toggle;
-  const AnimatedSwitcherExample({Key? key, required this.toggle})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedSwitcher(
-      duration: duration,
-      child: toggle
-          ? Container(
-              key: const ValueKey(0),
-              width: 50,
-              height: 50,
-              color: Colors.red,
-            )
-          : Container(
-              key: const ValueKey(1),
-              width: 150,
-              height: 150,
-              color: Colors.blue,
-            ),
-      transitionBuilder: (child, animation) => SizeTransition(
-        child: child,
-        sizeFactor: animation,
-      ),
-    );
-  }
-}
-
-// AnimatedSize
-
-class AnimatedSizeExample extends StatelessWidget {
-  final bool toggle;
-  const AnimatedSizeExample({Key? key, required this.toggle}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedSize(
-      duration: duration,
-      alignment: Alignment.topRight,
-      child: Container(
-        width: toggle ? 150 : 50,
-        height: toggle ? 150 : 50,
-        color: Colors.blue,
-      ),
-      curve: Curves.slowMiddle,
-    );
-  }
-}
-
-// AnimatedContainer
-
-class AnimatedContainerExample extends StatelessWidget {
-  final bool toggle;
-  const AnimatedContainerExample({Key? key, required this.toggle})
-      : super(key: key);
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return AnimatedContainer(
-  //     duration: duration,
-  //     color: Colors.red,
-  //     padding: const EdgeInsets.all(40),
-  //     child: Container(
-  //       width: toggle ? 150 : 50,
-  //       height: toggle ? 150 : 50,
-  //       color: Colors.blue,
-  //     ),
-  //   );
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: duration,
-      color: toggle ? Colors.red : Colors.green,
-      padding: toggle ? const EdgeInsets.all(40) : const EdgeInsets.all(80),
-      width: toggle ? 100 : 400,
-      child: Container(
-        width: 50,
-        height: 50,
-        color: Colors.blue,
-      ),
-    );
-  }
-}
-
-class ContainerExample extends StatelessWidget {
-  final bool toggle;
-  const ContainerExample({
-    Key? key,
-    required this.toggle,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: toggle ? Colors.red : Colors.green,
-      padding: toggle ? const EdgeInsets.all(40) : const EdgeInsets.all(80),
-      width: toggle ? 100 : 400,
-      child: Container(
-        width: 50,
-        height: 50,
-        color: Colors.blue,
       ),
     );
   }
